a9e4ca88e408727a75d78b2c4d932b886e4807c9
zuhanit <36349353+zuhanit@users.noreply.github.com>
2021-05-16 17:01:57 +0900

    Support parameter types

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -1,5 +1,7 @@
+import { NONAME } from "dns";
 import * as vscode from "vscode";
 import { functions } from "./signature/functions";
+import { params } from "./signature/params";
 
 export class EPSCompletionItemProvider implements vscode.CompletionItemProvider {
     public provideCompletionItems(
@@ -8,12 +10,47 @@ export class EPSCompletionItemProvider implements vscode.CompletionItemProvider 
         token: vscode.CancellationToken, 
         context: vscode.CompletionContext
     ) {
+        const totalWord = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position));
+        let paramCount: number = 0;
+        let methodName: string = "";
+        let isInFunctionScope: boolean = false;
         const completionItemList = [];
-        for (let i in functions) {
-            const completion = new vscode.CompletionItem(i);
-            completionItemList.push(completion);
+
+        for (let i = 0; i < totalWord.length; i++) {
+            let currentCharacter: string = totalWord.charAt(i);
+            if (currentCharacter === '(') {
+                const methodNameRange = document.getWordRangeAtPosition(new vscode.Position(position.line, i-1));
+                methodName = document.getText(methodNameRange);
+                isInFunctionScope = true;
+                break;
+            }
         }
 
+        if (isInFunctionScope === true) {
+            for (let k = 0; k < totalWord.length; k++) {
+                let currentCharacter: string = totalWord.charAt(k);
+                if (currentCharacter === ',') {
+                    paramCount += 1;
+                }
+                if (currentCharacter === ')') {
+                    return undefined;
+                }
+            }
+            const currentFunctionParams = functions[methodName as keyof typeof functions];
+            const currentParameterName = currentFunctionParams.params[paramCount].name;
+            const currentParameterTypes = params[currentParameterName as keyof typeof params].type;
+            for (let i in currentParameterTypes) {
+                const parameterTypesName = `"${currentParameterTypes[i]}"`;
+                const completion = new vscode.CompletionItem(parameterTypesName);
+                console.log(completion);
+                completionItemList.push(completion);
+            }
+        } else if(isInFunctionScope === false) {
+            for (let i in functions) {
+                const completion = new vscode.CompletionItem(i);
+                completionItemList.push(completion);
+            }
+        }
         return completionItemList;
     }
 }
--- src/signature/params.ts
+++ src/signature/params.ts
@@ -1,92 +1,178 @@
 export const params = {
     "Player": {
-        description: "Player Description"
+        description: "Player Description",
+        type: [
+            'P1', 'P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10', 'P11', 'P12',
+            'CurrentPlayer', 'Foes', 'Allies', 'NeutralPlayers', 'Force1', 'Force2', 'Force3', 'Force4',
+            'NonAlliesVictoryPlayers'
+        ],
     },
     "Comparison": {
-        description: "Comparison Description"
+        description: "Comparison Description",
+        type: [
+            'AtLeast', 'AtMost', 'Exactly'
+        ]
     },
     "Number": {
-        description: "Number Description"
+        description: "Number Description",
+        type: [
+            '0'
+        ]
     },
     "ResourceType": {
-        description: "ResourceType Description"
+        description: "ResourceType Description",
+        type: [
+            'Ore', 'Gas', 'OreAndGas'
+        ]
     },
     "Unit": {
-        description: "Unit Description"
+        description: "Unit Description",
+        type: [
+            'Terran Marine'
+        ]
     },
     "Location": {
-        description: "Location Description"
+        description: "Location Description",
+        type: [
+            'Location 1'
+        ]
     },
     "Time": {
-        description: "Time Description"
+        description: "Time Description",
+        type: [
+            '1'
+        ]
     },
     "ScoreType": {
-        description: "ScoreType Description"
+        description: "ScoreType Description",
+        type: [
+            'Units', 'Buildings', 'UnitsAndBuildings', 'TotalKills', 'Razings', 'KillsAndRazings', 'Custom'
+        ]
     },
     "Switch": {
-        description: "Switch"
+        description: "Switch",
+        type: [
+            'Switch 1'
+        ]
     },
     "State": {
-        description: "State"
-    },
-    "Where": {
-        description: "Where"
+        description: "State",
+        type: [
+            'Set', 'Cleared'
+        ]
     },
     "Text": {
-        description: "Text"
+        description: "Text",
+        type: [
+            'Hello World!'
+        ]
     },
     "ForPlayer": {
-        description: "ForPlayer"
+        description: "ForPlayer",
+        type: [
+            'P1'
+        ]
     },
     "Count": {
-        description: "Count"
+        description: "Count",
+        type: [
+            '1'
+        ]
     },
     "Properties": {
-        description: "Properties"
+        description: "Properties",
+        type: [
+            '1'
+        ]
     },
     "Owner": {
-        description: "Owner"
+        description: "Owner",
+        type: [
+            'P1'
+        ]
     },
     "NewOwner": {
-        description: "NewOwner Description"
+        description: "NewOwner Description",
+        type: [
+            'P2'
+        ]
     },
     "Label": {
-        description: "Label Description"
+        description: "Label Description",
+        type: [
+            '1'
+        ]
     },
     "Goal": {
-        description: "Goal Description"
+        description: "Goal Description",
+        type: [
+            '?'
+        ]
     },
     "Percent": {
-        description: "Percent Description"
+        description: "Percent Description",
+        type: [
+            '1'
+        ]
     },
     "Add": {
-        description: "Add Description"
+        description: "Add Description",
+        type: [
+            'Add'
+        ]
     },
     "NewValue": {
-        description: "NewValue Description"
+        description: "NewValue Description",
+        type: [
+            '1'
+        ]
     },
     "DestLocation": {
-        description: "DestLocation Description"
+        description: "DestLocation Description",
+        type: [
+            '2'
+        ]
     },
     "StartLocation": {
-        description: "StartLocation Description"
+        description: "StartLocation Description",
+        type: [
+            '3'
+        ]
     },
     "Script": {
-        description: "Script Description"
+        description: "Script Description",
+        type: [
+            '4'
+        ]
     },
     "TimeModifier": {
-        description: "TimeModifier Description"
+        description: "TimeModifier Description",
+        type: [
+            '5'
+        ]
     },
     "Modifier": {
-        description: "Modifier Description"
+        description: "Modifier Description",
+        type: [
+            'Add', 'SetTo', 'Subtract'
+        ]
     },
     "ScenarioName": {
-        description: "ScenarioName Description"
+        description: "ScenarioName Description",
+        type: [
+            '1'
+        ]
     },
     "Offset": {
-        description: "Offset Description"
+        description: "Offset Description",
+        type: [
+            '2'
+        ]
     },
     "Value": {
-        description: "Value Description"
+        description: "Value Description",
+        type: [
+            '3'
+        ]
     },
 };
