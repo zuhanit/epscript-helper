3338132c257cd83a140e9274eedb68658e6ad75c
zuhanit <36349353+zuhanit@users.noreply.github.com>
2021-05-17 15:38:56 +0900

    Support variable hover

--- src/hoverProvider.ts
+++ src/hoverProvider.ts
@@ -18,6 +18,11 @@ export class EPSHoverProvider implements vscode.HoverProvider {
             hoverText.appendMarkdown(description);
             const hover = new vscode.Hover(hoverText);
             return hover;
+        } else if(this.constChecker(word, document, position)) {
+            const hoverText = new vscode.MarkdownString();
+            hoverText.appendCodeblock(`const ${word}`, 'epscript');
+            const hover = this.constProvider(word, document, position);
+            return hover;
         }
         return null;
     }
@@ -25,5 +30,32 @@ export class EPSHoverProvider implements vscode.HoverProvider {
     private functionChecker(word: string) {
         const c = functions[word as keyof typeof functions];
         return c !== undefined? true : false; 
+    }
+
+    private constChecker(
+        word: string,
+        document: vscode.TextDocument,
+        position: vscode.Position
+    ) {
+        const regex = new RegExp('(\s*)(const|var)(\\s+)(' + word + ')(\\s*)(=)(\s*)(EUDArray|EUDVariable|EUDVarray|.*)(.*)');
+        const textUntilPosition = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
+        return regex.test(textUntilPosition);
+    }
+
+    private constProvider(
+        word: string,
+        document: vscode.TextDocument,
+        position: vscode.Position
+    ) {
+        const regex = new RegExp('(\s*)(const|var)(\\s+)(' + word + ')(\\s*)(=)(\s*)(EUDArray|EUDVariable|EUDVarray|.*)(.*)');
+        const textUntilPosition = document.getText(new vscode.Range(new vscode.Position(0, 0), position));
+        const m = textUntilPosition.match(regex);
+        if (!m) {
+            return;
+        }
+        const hoverText = new vscode.MarkdownString();
+        hoverText.appendCodeblock(`${m[2]} ${m[4]} ${m[6]} ${m[8]}`);
+        const hover = new vscode.Hover(hoverText);
+        return hover;
     }
 }
