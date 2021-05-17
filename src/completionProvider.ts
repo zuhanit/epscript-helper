a00d6c34a0e955cdfe0fa117f83aa55281649f37
zuhanit <36349353+zuhanit@users.noreply.github.com>
2021-05-17 15:40:19 +0900

    add scope checker

--- src/completionProvider.ts
+++ src/completionProvider.ts
@@ -2,7 +2,6 @@ import { NONAME } from "dns";
 import * as vscode from "vscode";
 import { functions } from "./signature/functions";
 import { params } from "./signature/params";
-
 export class EPSCompletionItemProvider implements vscode.CompletionItemProvider {
     public provideCompletionItems(
         document: vscode.TextDocument, 
@@ -11,41 +10,62 @@ export class EPSCompletionItemProvider implements vscode.CompletionItemProvider 
         context: vscode.CompletionContext
     ) {
         const totalWord = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position));
+        const lineText = document.lineAt(position.line).text;
         let paramCount: number = 0;
         let methodName: string = "";
-        let isInFunctionScope: boolean = false;
+        let inScope: boolean = false;
+        let leftClose: number = 0;
+        let rightClose: number = 0;
         const completionItemList = [];
 
+        // Check code is surrounded. if leftClose > rightClose => surrounded.
         for (let i = 0; i < totalWord.length; i++) {
             let currentCharacter: string = totalWord.charAt(i);
             if (currentCharacter === '(') {
-                const methodNameRange = document.getWordRangeAtPosition(new vscode.Position(position.line, i-1));
+                let methodNameRange = document.getWordRangeAtPosition(new vscode.Position(position.line, i-1));
                 methodName = document.getText(methodNameRange);
-                isInFunctionScope = true;
-                break;
+                leftClose += 1;
+            }
+            if (currentCharacter === ')') {
+                rightClose += 1;
             }
         }
+        if (leftClose > rightClose) {
+            inScope = true;
+        } else if (leftClose === rightClose) {
+            inScope = false;
+        }
 
-        if (isInFunctionScope === true) {
-            for (let k = 0; k < totalWord.length; k++) {
-                let currentCharacter: string = totalWord.charAt(k);
-                if (currentCharacter === ',') {
-                    paramCount += 1;
+        if (inScope === true) {
+            const k = functions[methodName as keyof typeof functions] === undefined? true : false;
+            if (k === true) {
+                for (let i in functions) {
+                    const completion = new vscode.CompletionItem(i);
+                    completion.kind = vscode.CompletionItemKind.Function;
+                    completion.documentation = functions[i as keyof typeof functions].description;
+                    completionItemList.push(completion);
                 }
-                if (currentCharacter === ')') {
-                    return undefined;
+            } else if(k === false) {
+                for (let k = 0; k < totalWord.length; k++) {
+                    let currentCharacter: string = totalWord.charAt(k);
+                    if (currentCharacter === ',') {
+                        paramCount += 1;
+                    }
+                    if (currentCharacter === ')') {
+                        return undefined;
+                    }
+                }
+                const currentFunctionParams = functions[methodName as keyof typeof functions];
+                const currentParameterName = currentFunctionParams.params[paramCount].name;
+                const currentParameterTypes = params[currentParameterName as keyof typeof params].type;
+                for (let i in currentParameterTypes) {
+                    const parameterTypesName: string = currentParameterName === 'Unit' ? `"${currentParameterTypes[i]}"` : `${currentParameterTypes[i]}`;
+                    const completion = new vscode.CompletionItem(parameterTypesName);
+                    completion.kind = vscode.CompletionItemKind.Constant;
+                    completionItemList.push(completion);
                 }
             }
-            const currentFunctionParams = functions[methodName as keyof typeof functions];
-            const currentParameterName = currentFunctionParams.params[paramCount].name;
-            const currentParameterTypes = params[currentParameterName as keyof typeof params].type;
-            for (let i in currentParameterTypes) {
-                const parameterTypesName: string = currentParameterName === 'Unit' ? `"${currentParameterTypes[i]}"` : `${currentParameterTypes[i]}`;
-                const completion = new vscode.CompletionItem(parameterTypesName);
-                completion.kind = vscode.CompletionItemKind.Constant;
-                completionItemList.push(completion);
-            }
-        } else if(isInFunctionScope === false) {
+        } else if(inScope === false) {
             for (let i in functions) {
                 const completion = new vscode.CompletionItem(i);
                 completion.kind = vscode.CompletionItemKind.Function;
