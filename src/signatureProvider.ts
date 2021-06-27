ee6a76ad3f80ccf61ab29190a3052b7b0a90d56d
zuhanit <36349353+zuhanit@users.noreply.github.com>
2021-06-28 04:02:30 +0900

    Add custom function completion

--- src/signatureProvider.ts
+++ src/signatureProvider.ts
@@ -1,7 +1,7 @@
-import { SIGHUP } from 'constants';
 import * as vscode from 'vscode';
 import { functions } from "./signature/functions";
 import { params } from "./signature/params";
+import getFileFunction from './parse/function';
 
     /*
     SetMemoryXEPD를 작성한다고 치자. SetMemoryXEPD(arg, arg, arg, arg)가 있다. 각 arg에 맞는 것이 있을 것임.
@@ -43,24 +43,45 @@ export class EPSSignatureHelpProvider implements vscode.SignatureHelpProvider {
                 }
             }
         }
-        const native = functions[methodName as keyof typeof functions];
-        const paramLength = native.params.length;
-        const paramArray = [];
-        for (let i = 0; i < paramLength; i++) {
-            const paramName = native.params[i].name;
-            const paramInfo = new vscode.ParameterInformation(paramName, params[paramName as keyof typeof params].description);
-            paramArray.push(paramInfo);
-        }
-        const paramList = native.params.map((u: any) => `${u.name}`).join(", ");
-        const signatureLabel: string = `${methodName}(${paramList})`;
+
+        const localFunctions = getFileFunction(document, position.line);
 
         const signatureHelp = new vscode.SignatureHelp();
-        signatureHelp.signatures = [];
-        const signatureInfo = new vscode.SignatureInformation(signatureLabel, native.description);
-        signatureHelp.signatures.push(signatureInfo);
-        signatureInfo.parameters = paramArray;
-        signatureHelp.activeSignature = 0;
-        signatureHelp.activeParameter = paramCount;
+        if (!!functions[methodName as keyof typeof functions]) { // need to refactoring. change functions type object to class and using filter to optimize.
+            const native = functions[methodName as keyof typeof functions];
+            const paramLength = native.params.length;
+            const paramArray = [];
+            for (let i = 0; i < paramLength; i++) {
+                const paramName = native.params[i].name;
+                const paramInfo = new vscode.ParameterInformation(paramName, params[paramName as keyof typeof params].description);
+                paramArray.push(paramInfo);
+            }
+            const paramList = native.params.map((u: any) => `${u.name}`).join(", ");
+            const signatureLabel: string = `${methodName}(${paramList})`;
+            signatureHelp.signatures = [];
+            const signatureInfo = new vscode.SignatureInformation(signatureLabel, native.description);
+            signatureHelp.signatures.push(signatureInfo);
+            signatureInfo.parameters = paramArray;
+            signatureHelp.activeSignature = 0;
+            signatureHelp.activeParameter = paramCount;
+        } else if (!!localFunctions.find(el => el.name === methodName)) {
+            const paramArray = [];
+            const func = localFunctions.find(el => el.name === methodName);
+            if (!!func?.params) {
+                for (const param of func.params) {
+                    const paramInfo = new vscode.ParameterInformation(param);
+                    paramArray.push(paramInfo);
+                }
+            }
+            const paramList = func?.params?.join(', ');
+            const signatureLabel: string = `${methodName}(${paramList})`;
+            const signatureInfo = new vscode.SignatureInformation(signatureLabel);
+            signatureInfo.parameters = paramArray;
+            signatureHelp.signatures = [];
+            signatureHelp.signatures.push(signatureInfo);
+            signatureHelp.activeParameter = paramCount;
+            signatureHelp.activeSignature = 0;
+        }
         return signatureHelp;
     }
 }
